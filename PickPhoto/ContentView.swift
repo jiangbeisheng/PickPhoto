43e905a33f308d91531bef83bdce1ab5ff778da5
lucas <beishengjiang@gmail.com>
2026-05-10 00:07:34 +0800

    Initial Commit

---
+++ PickPhoto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickPhoto
+//
+//  Created by lucasjiang on 2026/5/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickPhoto/PickPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickPhotoApp.swift
+//  PickPhoto
+//
+//  Created by lucasjiang on 2026/5/10.
+//
+
+import SwiftUI
+
+@main
+struct PickPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
